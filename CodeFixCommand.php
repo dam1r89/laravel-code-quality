c7d196686bbeb3bdec013197286cff386c765ba2
Damir Miladinov <damir89@gmail.com>
2018-07-16 16:57:25 +0200

    Work on past commits

--- CodeFixCommand.php
+++ CodeFixCommand.php
@@ -6,15 +6,18 @@ use Illuminate\Console\Command;
 
 class CodeFixCommand extends Command
 {
-    protected $signature = 'code:fix';
+    protected $signature = 'code:fix {--last-commit=1}';
 
     protected $description = 'Fix code style';
 
     public function handle()
     {
-        passthru('git diff --name-only --cached --diff-filter=ACMRTUXB | xargs php ./vendor/bin/php-cs-fixer fix --path-mode=intersection --config=.php_cs.dist  --using-cache=no', $status);
+        $files = 'git diff --name-only --cached --diff-filter=ACMRTUXB HEAD';
+        $files = $files . '~' . $this->option('last-commit');
 
-        passthru("./node_modules/eslint/bin/eslint.js --fix `git diff --name-only --cached --diff-filter=AM HEAD | grep '^resources/assets/js.*\.\(vue\|js\)\$'`", $status2);
+        passthru("$files | xargs php ./vendor/bin/php-cs-fixer fix --path-mode=intersection --config=.php_cs.dist  --using-cache=no", $status);
+
+        passthru("./node_modules/eslint/bin/eslint.js --fix `$files | grep '^resources/assets/js.*\.\(vue\|js\)\$'`", $status2);
 
         return (int) ($status || $status2);
     }
